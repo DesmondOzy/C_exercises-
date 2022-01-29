db2b0293c3d48da4d98feda0f15c3ea644274027
DesmondOzy <97857621+DesmondOzy@users.noreply.github.com>
2022-01-29 23:45:54 +0200

    Create mario_hard(not my).c

---
+++ mario_hard(not my).c
@@ -0,0 +1,44 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+   //Создаем переменные для высоты, пробелов и хэшей
+    int height;
+    int spaces;
+    int hashes;
+    int hashes2;
+
+do
+    {
+        height = get_int("Height: ");
+    }
+    //Устанавливаем условия
+    while (height < 1 || height > 8);
+    //Создаем строки
+    for (int i = 1; i <= height; i++)
+    {
+     //Создаем столбцы пробелов
+       for (spaces = (height - i); spaces > 0; spaces--)
+        {
+            printf("_");
+            sleep(1);
+        }
+         //Создаем столбцы хешей
+        for (hashes = 1; hashes <= (i); hashes++)
+        {
+            printf("#");
+            sleep(1);
+        }
+        printf("__");
+            //Создаем столбцы хешей
+        for (hashes2 = 1; hashes2 <= (i); hashes2++)
+        {
+            printf("#");
+            sleep(1);
+        }
+        printf("\n");
+     }
+    return 0;
+}
